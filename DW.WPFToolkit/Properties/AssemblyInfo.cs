7160e92db1a004c7d3aacacc7e3a5556be69b3be
David Wendland <david.w.dev@gmail.com>
2020-05-19 13:57:05 +0200

    Add assembly which enables the theming

---
+++ DW.WPFToolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.InteropServices;
+
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+[assembly: ComVisible(false)]
+[assembly: Guid("60e79efa-87dc-4535-ace5-2f503491b54a")]
